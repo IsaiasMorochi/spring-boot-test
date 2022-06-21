f78454d9fef4cc751de28af9ce132881545cbd1b
IsaiasMorochi <imorochi.dev@gmail.com>
2022-06-21 16:49:57 -0400

    Implementacion de clase AccountService

---
+++ src/main/java/com/imorochi/services/AccountServiceImpl.java
@@ -0,0 +1,52 @@
+package com.imorochi.services;
+
+import com.imorochi.model.Account;
+import com.imorochi.model.Bank;
+import com.imorochi.repositories.AccountRepository;
+import com.imorochi.repositories.BankRepository;
+
+import java.math.BigDecimal;
+
+public class AccountServiceImpl implements AccountService {
+
+    private AccountRepository accountRepository;
+    private BankRepository bankRepository;
+
+    public AccountServiceImpl(AccountRepository accountRepository, BankRepository bankRepository) {
+        this.accountRepository = accountRepository;
+        this.bankRepository = bankRepository;
+    }
+
+    @Override
+    public Account findById(Long id) {
+        return accountRepository.findById(id);
+    }
+
+    @Override
+    public int reviewFullTransfer(Long bankId) {
+        Bank bank = bankRepository.findById(bankId);
+        return bank.getFullTransfer();
+    }
+
+    @Override
+    public BigDecimal reviewSaldo(Long accountId) {
+        Account account = accountRepository.findById(accountId);
+        return account.getSaldo();
+    }
+
+    @Override
+    public void transfer(Long originAccountNumber, Long destinationAccountNumber, BigDecimal amount) {
+        Bank bank = bankRepository.findById(1L);
+        int fullTransfer = bank.getFullTransfer();
+        bank.setFullTransfer(fullTransfer);
+        bankRepository.update(bank);
+
+        Account originAccount = accountRepository.findById(originAccountNumber);
+        originAccount.debit(amount);
+        accountRepository.update(originAccount);
+
+        Account destinationAccount = accountRepository.findById(destinationAccountNumber);
+        destinationAccount.credit(amount);
+        accountRepository.update(destinationAccount);
+    }
+}
