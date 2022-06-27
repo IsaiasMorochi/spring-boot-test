ee585e28b710aac9f3feccea297ec4b560587e55
IsaiasMorochi <imorochi.dev@gmail.com>
2022-06-26 21:44:35 -0400

    80. Test Controllers : Prueba unitaria transferir monto

--- src/test/java/com/imorochi/controller/AccountControllerTest.java
+++ src/test/java/com/imorochi/controller/AccountControllerTest.java
@@ -1,12 +1,19 @@
 package com.imorochi.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.imorochi.model.dto.TransferDto;
 import com.imorochi.services.AccountService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import static com.imorochi.mock.Data.createdAccount001;
@@ -21,6 +28,13 @@ class AccountControllerTest {
 
     @MockBean
     private AccountService accountService;
+
+    ObjectMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        mapper = new ObjectMapper();
+    }
 
     @Test
     void test_find_account() throws Exception {
@@ -39,4 +53,24 @@ class AccountControllerTest {
         verify(accountService).findById(1L);
     }
 
+    @Test
+    void test_transfer_amount() throws Exception {
+        // Given
+        TransferDto dtoBuilder = TransferDto.builder()
+                .originAccountNumber(1L)
+                .destinationAccountNumber(2L)
+                .amount(new BigDecimal("100"))
+                .bankId(1L)
+                .build();
+        // When
+        mockMvc.perform(post("/api/accounts/transfer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(dtoBuilder)))
+        // Then
+        .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.date").value(LocalDate.now().toString()))
+                .andExpect(jsonPath("$.message").value("Transaccion realizada con exito."))
+                .andExpect(jsonPath("$.transactions.originAccountNumber").value(1L));
+    }
 }
